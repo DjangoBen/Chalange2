804437287ff07ce60d27e0f3b847a9cbf2b4123a
DjangoBen <92127895+DjangoBen@users.noreply.github.com>
2022-01-19 03:04:29 +0200

    Update settings.py

--- WelcomeForms/settings.py
+++ WelcomeForms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_9p@1*anli@r7qd^q2r0995-0=yqduz+xn_&xybq@lc4u5!rdh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
